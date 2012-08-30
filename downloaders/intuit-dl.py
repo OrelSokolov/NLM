e3b16d9b66bdbb2ca11f3814bc9bfbad1ed34965
Orel Sokolov <orelcokolov@gmail.com>
2012-08-30 23:14:23 +0300

    Добавлен загрузчик с intuit.ru

--- downloaders/intuit-dl.py
+++ downloaders/intuit-dl.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import HTMLParser
 import urllib
-import urlparse
+from os.path import basename
+from os.path import splitext
+import xml.dom.minidom as xml #Для парсинга xml файла с ссылкой на поток.
+import sys
+import subprocess as sps
 
-def findXMLpath(url):
-	'''Находит путь xml файла, который ведет к rtmp потоку.'''
-	pass
+class findXML(HTMLParser.HTMLParser):
+	'''Парсер для страницы с видео.'''
+	xml=None
+	url=None
+	def findVideoIn(self, url):
+		self.url=url
+		self.feed(urllib.urlopen(url).read())
+	def handle_data(self, data):
+		if data.find("show_video")!=-1: 
+			print "The video has been detected."
+			#print data
+			if data.find('&')!=-1:
+				self.xml=self.url+basename(data[ data.find("'")+1 : data.rfind("&") ])
+			elif data.find('?')!=-1:
+				self.xml=self.url+basename(data[ data.find("'")+1 : data.rfind("?") ])
+			else:
+				self.xml=self.url+basename(data[ data.find("'")+1 : data.rfind("'") ])
+			
+
+def getChannel(text):
+	if text.find("clip")!=-1:
+		 return text[:text.rfind(".")]+'M'
+	else:
+		return text[:text.rfind(".")]+'L'
 
 
 
-	
-
+if __name__=='__main__':
+	p=findXML()
+	try:	p.findVideoIn(sys.argv[1])
+	except IndexError:
+		print "Не передана ссылка."
+		raise SystemExit
+	p=xml.parseString(urllib.urlopen(p.xml).read())
+	channel=getChannel(p.getElementsByTagName("resource")[0].getAttribute("source"))# Передалать в более универсальный случай
+	sps.call(['rtmpdump', '-q', '-r', channel, '-o', 'video.flv'])
