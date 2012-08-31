559bad035c5fe46c7f928a7e33ee0eb52f372a15
Orel Sokolov <orelcokolov@gmail.com>
2012-08-31 14:55:03 +0300

    Полностью дописан загрузчик intuit-dl.py

--- downloaders/intuit-dl.py
+++ downloaders/intuit-dl.py
@@ -5,20 +5,37 @@ import HTMLParser
 import urllib
 from os.path import basename
 from os.path import splitext
+from os.path import exists
 import xml.dom.minidom as xml #Для парсинга xml файла с ссылкой на поток.
 import sys
 import subprocess as sps
+import time
+
+if not exists('/usr/bin/rtmpdump'):
+	print "Установите rtmpdump!"
+	raise SystemExit
 
 class findXML(HTMLParser.HTMLParser):
-	'''Парсер для страницы с видео.'''
+	'''Парсер для страницы с видео. Читайте вики-документацию'''
 	xml=None
 	url=None
+	title=str(time.time())
+	title_found=False
+	current_starttag={}
+	div_closed=False
 	def findVideoIn(self, url):
 		self.url=url
 		self.feed(urllib.urlopen(url).read())
+	def handle_starttag(self, tag, attr):
+		self.current_starttag=self.get_starttag_text()
+		if self.get_starttag_text()=='<div class="lecture_title">':
+			self.div_closed=False
+	def handle_endtag(self, tag):
+		if self.get_starttag_text()=='<div class="lecture_title">'and tag=='div':
+			self.div_closed=True #Если тег div вложен. Иначе будем обрабатывать не те данные, которые нужны (еще и несколько раз).
 	def handle_data(self, data):
 		if data.find("show_video")!=-1: 
-			print "The video has been detected."
+			print "Видео обнаружено.",
 			#print data
 			if data.find('&')!=-1:
 				self.xml=self.url+basename(data[ data.find("'")+1 : data.rfind("&") ])
@@ -26,7 +43,22 @@ class findXML(HTMLParser.HTMLParser):
 				self.xml=self.url+basename(data[ data.find("'")+1 : data.rfind("?") ])
 			else:
 				self.xml=self.url+basename(data[ data.find("'")+1 : data.rfind("'") ])
-			
+		if self.get_starttag_text()=='<div class="lecture_title">' and self.div_closed==False: 
+			if(self.title_found==False):
+				print "Название лекции найдено:",
+				self.title=data.decode('CP1251')
+				while self.title.find('\n')!=-1: # На всякий случай
+					i=self.title.find('\n')
+					self.title=self.title[:i]+self.title[i+1:]
+				self.title_found=True
+				print self.title
+			else:
+				line, pos=self.getpos()
+				#if len(data)>len(self.title):
+				print "FIXME: Другое название было обнаружено в строке", line, "на позиции", pos, "длинной:", len(data)
+
+	def getTitle(self):
+		return self.title
 
 def getChannel(text):
 	if text.find("clip")!=-1:
@@ -34,14 +66,19 @@ def getChannel(text):
 	else:
 		return text[:text.rfind(".")]+'L'
 
-
-
-if __name__=='__main__':
+def downloadVideoFrom(url):	
+	'''Загружает видео с переданного url'''
 	p=findXML()
-	try:	p.findVideoIn(sys.argv[1])
-	except IndexError:
-		print "Не передана ссылка."
-		raise SystemExit
+	p.findVideoIn(url)
+	title=p.getTitle()
 	p=xml.parseString(urllib.urlopen(p.xml).read())
 	channel=getChannel(p.getElementsByTagName("resource")[0].getAttribute("source"))# Передалать в более универсальный случай
-	sps.call(['rtmpdump', '-q', '-r', channel, '-o', 'video.flv'])
+	print "Загрузка начата."
+	try: sps.call(['rtmpdump', '-q', '-r', channel, '-o', title+'.flv'])
+	except KeyboardInterrupt: print "Прервано пользователем."; raise SystemExit
+	print "Вроде загрузилось."
+
+if __name__=='__main__':
+	try: downloadVideoFrom(sys.argv[1])
+	except IndexError: 
+		print "Не передана ссылка."; raise SystemExit
